663d7a9a9ab187acb88cfa4284ace7fe9936cd0d
adamshelby <adam_shelby@brown.edu>
2021-11-15 13:59:54 -0500

    Exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,18 +8,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      {props.part} {props.exercises}
+    </div>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   )
 }
@@ -44,9 +46,9 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part1="Fundamentals of React" exercises1={10}
-        part2="Using props to pass data" exercises2={7}
-        part3="State of a component" exercises3={14} />
+      <Content part1={part1} exercises1={10}
+        part2={part2} exercises2={7}
+        part3={part3} exercises3={14} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
