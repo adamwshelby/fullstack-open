96be9de81a31e0be171c67bf6494454aa2a365e5
adamshelby <adam_shelby@brown.edu>
2021-11-18 14:06:34 -0500

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,38 @@
 import React, { useState } from 'react'
 
+const Numbers = ({ filter, persons }) => {
+
+  const isPrefix = (p, s) => {
+    if (p.length <= s.length) {
+      return (s.substring(0, p.length).toUpperCase() === p.toUpperCase())
+    } else {
+      return false
+    }
+  }
+
+  if (filter.length === 0) {
+    return (
+      <div>{persons.map((p) => <div key={p.name}>{p.name} {p.number}</div>)}</div>
+    )} else {
+    return(
+      <div>{persons.map((p) => isPrefix(filter, p.name) ? <div key={p.name}>{p.name} {p.number}</div> : <div key={p.name}></div>)}</div>
+      )
+  }
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' , number: '1234567890'}
+    { name: 'Arto Hellas' , number: '1234567890'},
+    { name: 'Arden Thomas', number: '09444'},
+    { name: 'Artie Gall', number: '914-907-1569'},
+    { name: 'Beatrice Bell', number: '628-13444-03'},
+    { name: 'Beatrice Bellie', number: '628-13444-032'},
+    { name: 'Beatrice Belloo', number: '628-13444-033'},
+    { name: 'Beatrice Bello3', number: '628-13444-034'}
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -15,8 +42,12 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   const personsEqual = (p1, p2) => {
-    return (p1.name === p2.name)
+    return (p1.name.toUpperCase() === p2.name.toUpperCase())
   }
 
   const addPerson = (event) => {
@@ -41,6 +72,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+        filter shown with <input onChange={handleFilterChange}/>
+      <h2>Add people</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input id="name" onChange={handleNameChange}/>
@@ -53,8 +86,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((p) => <div key={p.name}>{p.name} {p.number}</div>
-      )}
+      <Numbers filter={filter} persons={persons}/>
     </div>
   )
 }
